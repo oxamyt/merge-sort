5d41691df9889e66ea82d945ecbff9ee401c0f59
Oxamyt <dentez.danil@gmail.com>
2024-02-11 20:04:47 +0200

    create-merge-sort-func

---
+++ script.js
@@ -0,0 +1,26 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  [2, 3];
+  const right = mergeSort(array.slice(mid));
+  [1, 13];
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const mergedArray = [];
+
+  while (left.length > 0 && right.length > 0) {
+    const arrayMin = left[0] < right[0] ? left : right;
+    const mergeElement = arrayMin.shift();
+    mergedArray.push(mergeElement);
+  }
+
+  return mergedArray.concat(left, right);
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
